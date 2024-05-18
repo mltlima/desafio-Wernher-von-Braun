d7d2c60b20c73840c0fc9b4b6b8c6331cab5a748
mltlima <miguellimatavares@gmail.com>
2024-05-18 19:11:00 -0300

    feat: create user model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword: string) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export default model<IUser>('User', UserSchema);
