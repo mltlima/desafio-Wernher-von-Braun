8de6464e341f9d49e6e34c8e97ae88b832780557
mltlima <miguellimatavares@gmail.com>
2024-05-18 19:11:56 -0300

    feat: create device model

---
+++ backend/src/models/Device.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface ICommand {
+  command: string;
+  parameters: string[];
+}
+
+interface IDevice extends Document {
+  identifier: string;
+  description: string;
+  manufacturer: string;
+  url: string;
+  commands: ICommand[];
+}
+
+const CommandSchema = new Schema<ICommand>({
+  command: { type: String, required: true },
+  parameters: [{ type: String }]
+});
+
+const DeviceSchema = new Schema<IDevice>({
+  identifier: { type: String, required: true },
+  description: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  url: { type: String, required: true },
+  commands: [CommandSchema]
+});
+
+export default model<IDevice>('Device', DeviceSchema);
